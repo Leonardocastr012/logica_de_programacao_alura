8a158e922fba43cdb509db17b6278e5db6b8f489
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-20 22:26:24 -0400

    I finished the first module of the programming logic course

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,6 @@
-alert('Boas vindas ao jogondo número secreto');
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número de 1 a 30: ');
+if (numeroSecreto == chute){
+    console.log('Isso aí! Você descobriu o número secreto (5)');
+}
