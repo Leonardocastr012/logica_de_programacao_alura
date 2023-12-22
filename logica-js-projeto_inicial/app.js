cf7e29144663a26acc16fa836f797b6778077858
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-22 15:48:06 -0400

    I finished the class about conditional and using of console.log

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 2;
+console.log('O número secreto é o ' + numeroSecreto);
 let chute = prompt('Escolha um número de 1 a 30: ');
+console.log('Valor do chute: '+ chute);
+
+// condicionais
+console.log('Resultado da comparação: ', numeroSecreto == chute);
 if (numeroSecreto == chute){
-    console.log('Isso aí! Você descobriu o número secreto (5)');
+    //alert('Isso aí! Você descobriu o número secreto é o' + numeroSecreto); 
+    alert(`Isso aí! Você descobriu o número secreto é o ${numeroSecreto}`); //Usando Template Strings
+}
+else {
+    console.log('Valor do número secreto:'+ numeroSecreto);
+    alert('Você errou :(');
 }
