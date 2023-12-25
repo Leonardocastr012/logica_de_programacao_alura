524e95d912824d64d71c02af981c454776983334
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-25 19:47:01 -0400

    I finished the class about loops and attempts

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,16 +1,25 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 2;
 console.log('O número secreto é o ' + numeroSecreto);
-let chute = prompt('Escolha um número de 1 a 30: ');
+let chute; 
+let tentativas = 1;
 console.log('Valor do chute: '+ chute);
-
-// condicionais
-console.log('Resultado da comparação: ', numeroSecreto == chute);
-if (numeroSecreto == chute){
-    //alert('Isso aí! Você descobriu o número secreto é o' + numeroSecreto); 
-    alert(`Isso aí! Você descobriu o número secreto é o ${numeroSecreto}`); //Usando Template Strings
-}
-else {
-    console.log('Valor do número secreto:'+ numeroSecreto);
-    alert('Você errou :(');
-}
+//loop
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número de 1 a 30: ');
+    // condicionais
+    console.log('Resultado da comparação: ', numeroSecreto == chute);
+    if (numeroSecreto == chute){
+        //alert('Isso aí! Você descobriu o número secreto é o' + numeroSecreto); 
+        alert(`Isso aí! Você descobriu o número secreto é o ${numeroSecreto} e teve ${tentativas} tentativa(s)`); //Usando Template Strings
+    }
+    else {
+        console.log('Valor do número secreto:'+ numeroSecreto);
+        if (numeroSecreto > chute){
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+    } 
+    tentativas += 1; // or tentativas ++;
+}   
