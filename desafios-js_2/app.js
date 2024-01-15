32fafa5786cc0d02b8e73f6e82959a80bd829a1f
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-01-15 18:52:23 -0400

    I finished the second challenge of the second course

--- desafios-js_2/app.js
+++ desafios-js_2/app.js
@@ -1,3 +1,4 @@
+//DESAFIO 1
 let tituloDesafio = document.querySelector('h1');
 tituloDesafio.innerHTML = 'Hora do desafio';
 
@@ -21,3 +22,45 @@ function botaoSoma(){
 }
 
 //let permite que você declare variáveis limitando seu escopo no bloco, instrução, ou em uma expressão na qual ela é usada. Isso é inverso da keyword var (en-US), que define uma variável globalmente ou no escopo inteiro de uma função, independentemente do escopo de bloco.
+
+//DESAFIO 2
+//1.Criar uma função que exibe "Olá, mundo!" no console.
+function olaMundo(){
+    console.log('Olá, mundo!');
+}
+olaMundo()
+//2.Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function olaNome(nome){
+    console.log(`Olá, ${nome}!`);
+}
+olaNome('Leonardo');
+//3.Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function dobro(num){
+    return num * 2 //console.log(`O dobro de ${num} é ${num*2}`)
+}
+let resultado = dobro(2);
+console.log(resultado);
+//4.Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function media(num1, num2, num3){
+    return (num1 + num2 + num3)/3
+    //console.log(`A média entre ${num1},${num2} e ${num3} é ${media}`)
+}
+let resultadoMedia = media(2,3,4);
+console.log(resultadoMedia);
+//5.Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maiorNum(a, b){
+    if(a > b) {
+        return a
+    } else {
+        return b
+    }
+    //return a > b ? a : b;
+}
+let maiorValor = maiorNum(9,11);
+console.log(maiorValor);
+//6.Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+function quadrado(num){
+    return num * num
+}
+let resultadoQuadrado = quadrado(3)
+console.log(resultadoQuadrado)
