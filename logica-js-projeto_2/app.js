ec2ecc1b61a20e97f116725936dd2d8188123979
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-02-04 15:09:35 -0400

    I created the function limparCampo and I activated the bottom Novo Jogo

--- logica-js-projeto_2/app.js
+++ logica-js-projeto_2/app.js
@@ -15,7 +15,8 @@ function verificarChute(){
                 exibirTextoNaTela('h1','Acertou!');
                 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
                 let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
-                exibirTextoNaTela('p', mensagemTentativas)  
+                exibirTextoNaTela('p', mensagemTentativas)
+                document.getElementById('reiniciar').removeAttribute('disabled'); //Através do ID vai ativar acessar o botão Novo Jogo
         } else {
                 if (chute > numeroSecreto) {
                         exibirTextoNaTela('h1', 'O número sereto é menor!');
@@ -23,9 +24,15 @@ function verificarChute(){
                         exibirTextoNaTela('h1', 'O número secreto é maior!');
                 }
                 tentativas++;
+                limparCampo();//Vai server para toda vez que fizer o chute o número anterior não ficar no campo de digitar o número
         }
 }
 
 function gerarNumeroAleatorio() {
         return parseInt(Math.random() * 10 + 1)
+}
+
+function limparCampo() {
+        chute = document.querySelector('input');
+        chute.value = '';
 }
