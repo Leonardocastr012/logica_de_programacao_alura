ec92ea9133c4b4ec73f5cec476ea56cc6552bb71
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-02-04 15:50:00 -0400

    I finished the code that I used functions to improve the experience of the player

--- logica-js-projeto_2/app.js
+++ logica-js-projeto_2/app.js
@@ -4,9 +4,12 @@ function exibirTextoNaTela(tag, texto){
         let campo = document.querySelector(tag);
         campo.innerHTML = texto;
 }
+function exibirMensagemNaTela(){
+        exibirTextoNaTela('h1','Jogo do número secreto')
+        exibirTextoNaTela('p', 'Esscolha um número entre 1 e 10')
+}
 
-exibirTextoNaTela('h1','Jogo do número secreto')
-exibirTextoNaTela('p', 'Esscolha um número entre 1 e 10')
+exibirMensagemNaTela()
 
 function verificarChute(){
         let chute = document.querySelector('input').value; //pois não é um texto e sim valor numérico
@@ -19,7 +22,7 @@ function verificarChute(){
                 document.getElementById('reiniciar').removeAttribute('disabled'); //Através do ID vai ativar acessar o botão Novo Jogo
         } else {
                 if (chute > numeroSecreto) {
-                        exibirTextoNaTela('h1', 'O número sereto é menor!');
+                        exibirTextoNaTela('h1', 'O número secreto é menor!');
                 } else {
                         exibirTextoNaTela('h1', 'O número secreto é maior!');
                 }
@@ -35,4 +38,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
         chute = document.querySelector('input');
         chute.value = '';
+}
+
+function reiniciarJogo() {
+        numeroSecreto = gerarNumeroAleatorio();
+        limparCampo();
+        tentativas = 1;
+        exibirMensagemNaTela()
+        document.getElementById('reiniciar').setAttribute('disabled', true) //Manter o 'disabled' desabilitado(redundante mas é isso)
 }
