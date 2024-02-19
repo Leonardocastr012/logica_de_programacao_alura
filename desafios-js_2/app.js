9360e35313c4b695fe98826feb802dc95e7003dc
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2024-02-19 17:15:31 -0400

    I finished the 4th challenge

--- desafios-js_2/app.js
+++ desafios-js_2/app.js
@@ -116,4 +116,20 @@ function mostrarTabuada(numero) {
       console.log(`${numero} x ${i} = ${resultado}`);
     }
   }
-  
+//DESAFIO 4
+//1.Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+//2.Crie uma lista de linguagens de programação chamada linguagensDeProgramacao.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Java', 'Python'];
+//3.Adicione à lista linguagensDeProgramacao os seguintes elementos: Java, Ruby e GoLang.
+let linguagensDeProgramacao2 = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao2.push('Java', 'Ruby', 'GoLang');
+//4.Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+//5.Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+//6.Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
